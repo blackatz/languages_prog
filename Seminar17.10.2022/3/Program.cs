0ac19bf79c7013a9bf5b779f40e5cea488accc14
blackatz <ksenya.noskova.98@inbox.ru>
2022-10-17 22:09:02 +0300

    Seminar1

--- Seminar17.10.2022/3/Program.cs
+++ Seminar17.10.2022/3/Program.cs
@@ -3,8 +3,13 @@
 Console.WriteLine("Программа вычисления Дня недели.");
 Console.WriteLine("Введите число дня: ");
 int input1 = int.Parse(Console.ReadLine()!);
-    if (input1 == 1) Console.WriteLine("Это понедельник");
+
+if (0 < input1 && input1 < 8) 
+  { if (input1 == 1) Console.WriteLine("Это понедельник");
     if (input1 == 2) Console.WriteLine("Это вторник");
     if (input1 == 3) Console.WriteLine("Это среда");
     if (input1 == 4) Console.WriteLine("Это четверг");
     if (input1 == 5) Console.WriteLine("Это пятница");
+  }
+  else 
+  Console.WriteLine("Такого дня нет");
