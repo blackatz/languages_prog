7dd7abb3e890356365a38acd0bfe42ba9b413c8a
blackatz <ksenya.noskova.98@inbox.ru>
2022-10-22 13:52:07 +0300

    new seminar

---
+++ Seminar2Csharp/1/Program.cs
@@ -0,0 +1,21 @@
+﻿//напишите программу, которая выводит случайное число из отрезка 10,99 
+//и показывает наибольшую цифру числа
+
+// генерация случайных чисел
+
+int random = new Random().Next(10, 100);
+
+// найти наибольшую цифру числа
+
+int firstnumber = random / 10;
+int secondnumber = random %10; //остаток от деления на 10
+
+Console.WriteLine(random);
+
+// показать наибольшую цифру числа
+
+int max = secondnumber;
+
+if (firstnumber > secondnumber) max = firstnumber;
+
+Console.WriteLine("Наибольшая цифра числа: " + max);
