2b6412ebc891ea168b607999c406d710d5543ff3
blackatz <ksenya.noskova.98@inbox.ru>
2022-10-22 14:30:21 +0300

    new seminar

--- Seminar2Csharp/1/Program.cs
+++ Seminar2Csharp/1/Program.cs
@@ -1,21 +1,30 @@
 ﻿//напишите программу, которая выводит случайное число из отрезка 10,99 
 //и показывает наибольшую цифру числа
 
+int GenerateRandomNumber() 
+{
+    return new Random().Next(10, 100);
+}
+
+int GetMaxDigit(int random) 
+{
+    int firstnumber = random / 10; //деление на 10, десятки
+    int secondnumber = random %10; //остаток от деления на 10, единицы
+
+    // показать наибольшую цифру числа
+
+    int max = secondnumber;
+
+    if (firstnumber > secondnumber) max = firstnumber;
+    return max;
+}
+
 // генерация случайных чисел
 
-int random = new Random().Next(10, 100);
+int random = GenerateRandomNumber();
+Console.WriteLine("Сгенерировано число " + random);
 
 // найти наибольшую цифру числа
-
-int firstnumber = random / 10;
-int secondnumber = random %10; //остаток от деления на 10
-
-Console.WriteLine(random);
-
-// показать наибольшую цифру числа
-
-int max = secondnumber;
-
-if (firstnumber > secondnumber) max = firstnumber;
+int max = GetMaxDigit(random);
 
 Console.WriteLine("Наибольшая цифра числа: " + max);
---
+++ Seminar2Csharp/2/Program.cs
@@ -0,0 +1,22 @@
+﻿// напишите программу, которая выводит случайное трехзначное число
+// и удаляет вторую цифру этого числа, например 456 = 46
+
+int GenerateRandomNumber() 
+{
+    return new Random().Next(100, 1000);
+}
+
+int Number(int random) 
+{
+    int firstnumber = random / 100; //количество сотен
+    int secondnumber = random %100; //остаток от деления на 10, количество единиц в числе
+
+    return random;
+}
+
+int random = GenerateRandomNumber();
+int number = Number(random);
+
+Console.WriteLine("Сгенерировано число " + random);
+Console.WriteLine("Сделано число " + number);
+
